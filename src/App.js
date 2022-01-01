eaae9c0d86684de8233cfcf404956d71a99480d6
QeNo <moodiiy.98@gmail.com>
2022-01-01 14:28:51 +0200

    move pages static to dynamic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
 import HomePage from './pages/homepage/HomePage.component';
+import ShopPage from './pages/shop/shop.component';
 import Error from './pages/error/error.component';
-
-const HatsPage = () => (
-    <div>
-        <h1>Hats Page</h1>
-    </div>
-);
-
-const JacketsPage = () => (
-    <div>
-        <h1>jackets</h1>
-    </div>
-)
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact path='/' component={HomePage} />
-                <Route path='/shop/hats' component={HatsPage} />
-                <Route path='/shop/Jackets' component={JacketsPage} />
+                <Route exact path='/shop' component={ShopPage} />
                 <Route path="*" component={Error} />
             </Switch>
         </div>
