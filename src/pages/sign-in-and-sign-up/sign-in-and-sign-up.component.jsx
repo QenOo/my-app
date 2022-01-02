0cdbdae2c8f6db1e220f78ecf66cc8bf60e4b9bc
QeNo <moodiiy.98@gmail.com>
2022-01-02 19:12:47 +0200

    add sign in and sign up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,5 +1,6 @@
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+
 const SignInAndSignUp = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
