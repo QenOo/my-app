86145205aaaf37c098d20c8d812e7322f03611a7
QeNo <moodiiy.98@gmail.com>
2022-01-04 15:54:07 +0200

    add if condition sign in & sign out

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,19 +1,30 @@
 import './header.styles.scss'
-
+import React from 'react';
 import { Link } from 'react-router-dom'
 import {ReactComponent as MyLogo} from '../../assets/crown.svg';
-const Header = () => (
-    <div className="header">
-        <Link className="logo-container" to="/">
-            <MyLogo className="logo" />
-        </Link>
-        <div className="options">
-            <Link className="option" to="/">Home</Link>
-            <Link className="option" to="/shop">Shop Page</Link>
-            <Link className="option" to="/contact">Contact</Link>
-            <Link className="option" to="/signin">Sign In</Link>
+
+import auth from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
+    <>
+        <div className="header">
+            <Link className="logo-container" to="/">
+                <MyLogo className="logo" />
+            </Link>
+            <div className="options">
+                <Link className="option" to="/">Home</Link>
+                <Link className="option" to="/shop">Shop Page</Link>
+                <Link className="option" to="/contact">Contact</Link>
+                {
+                    currentUser ? 
+                        <Link className="option" to="" onClick={() => auth.signOut()} >Sign out</Link>
+                    : 
+                        <Link className="option" to="/signin">Sign In</Link>
+                }
+            </div>
         </div>
-    </div>
+        {console.log(currentUser)}
+    </>
 )
 
 export default Header;
