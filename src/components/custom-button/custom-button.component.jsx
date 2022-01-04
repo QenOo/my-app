b8bfb9597157aefc508e3c59247162d753bc89f6
QeNo <moodiiy.98@gmail.com>
2022-01-04 15:53:38 +0200

    add class if sign in with google to custom button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,7 +1,7 @@
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherItemProps }) => (
-    <button className="custom-button" {...otherItemProps} >
+const CustomButton = ({ children, isGoogleSignIn, ...otherItemProps }) => (
+    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherItemProps} >
         {children}
     </button>
 )
