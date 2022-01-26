dd8ed34c3a37f33f44685daa7a09733425a4678a
QeNo <moodiiy.98@gmail.com>
2022-01-26 17:56:23 +0200

    add custom button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,18 +1,20 @@
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div 
-            className="image"
-            style={{ 
-                backgroundImage: `url(${imageUrl})` 
+    <div className='collection-item'>
+        <div
+            className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`,
             }}
         />
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
         </div>
+        <CustomButton> Add to card </CustomButton>
     </div>
-)
+);
 
 export default CollectionItem;
